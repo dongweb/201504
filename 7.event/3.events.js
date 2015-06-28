de7751422704e67e454933f255b06017e3651306
zhangrenyang-t510 <zhang_renyang@>
2015-06-28 12:53:14 +0800

    eventemitter

--- 7.event/3.events.js
+++ 7.event/3.events.js
@@ -1,45 +1,54 @@
+//var EventEmitter = require('events').EventEmitter;
 
-
-function Emitter(){
+function Event(){
 
 }
 
-Emitter.prototype.on =
-    function on(type,listener){
-    if(typeof listener != 'function'){
+Event.prototype.addListener = function addListener(type, listener) {
+    if (typeof listener !== 'function')
         throw TypeError('listener must be a function');
-    }
-    //如果此属性不存在，则进行初始化
-    if(!this._events){
+    if (!this._events)
         this._events = {};
-    }
-    if(this._events[type]){
+    if (this._events[type])
         this._events[type].push(listener);
-    }else{
+    else
         this._events[type] = [listener];
-    }
 }
 
-Emitter.prototype.emit = function emit(type){
-    if(!this._events){
+Event.prototype.emit = function emit(type) {
+    if (!this._events)
         this._events = {};
-    }
-    var listeners = this._events[type];
-    if(listeners)
-    listeners.forEach(function(listener){
-        listener.apply(this);
-    })
+    var handler = this._events[type];
+
+    var arr = new Array(arguments.length -1);
+    for(var i=1;i<arguments.length;i++)
+        arr[i-1] = arguments[i];
+
+    for (var j = 0; j < handler.length; j++)
+        handler[j].apply(this, arr);
 }
 
 function Girl(){
 
 }
-Girl.prototype = new Emitter();
-
+Girl.prototype = new Event();
+//构建女神，好我人追哦
 var girl = new Girl();
+function Boy(){
 
-girl.on('21years',function(){
-    console.log('marry my son');
-})
-girl.emit('21years');
+}
+var tuhao = new Boy();
+tuhao.comfort = function(thing){
+    console.log('tuhao:喜欢吗？喜欢我就买'+thing+'给你');
+}
+girl.addListener('看了好久好久',tuhao.comfort);
 
+var diaosi = new Boy();
+diaosi.comfort = function(thing){
+    console.log('diaosi:喜欢吗？喜欢就多看'+thing+'一会');
+}
+girl.addListener('看了好久好久',diaosi.comfort);
+
+
+girl.emit('看了好久好久','apple watch');
+//凤姐没人追
