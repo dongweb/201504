2eeba63441735565281f14679f80c19c027eff87
zhangrenyang-t510 <zhang_renyang@>
2015-07-11 18:10:52 +0800

    多语言

--- 17.http2/1.server_refer.js
+++ 17.http2/1.server_refer.js
@@ -2,6 +2,8 @@ var http = require('http');
 var url = require('url');
 var fs = require('fs');
 /**
+ * 从一个网页跳转，或者网页引用一个资源的时候，
+ * HTTP请求的会带有refer表示来源网页的URL
  *
  */
 var server = http.createServer(function(req,res){
@@ -9,8 +11,16 @@ var server = http.createServer(function(req,res){
     var pathname = urlObj.pathname;
     if(pathname =='/'){
         fs.createReadStream('./img.html').pipe(res);
-    }else if(pathname =='/post'){
-
+    }else if(pathname =='/logo.jpg'){
+        var referer = req.headers['referer'];
+        if (referer){
+            var host = url.parse(referer).host.split(':')[0];
+            if(host == 'b.zfpx.com'){
+                fs.createReadStream('./no.jpg').pipe(res);
+                return;
+            }
+        }
+        fs.createReadStream('./logo.jpg').pipe(res);
     }
 
 }).listen(8080);
---
+++ 17.http2/2.multi.js
@@ -0,0 +1,43 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+/**
+ * 从一个网页跳转，或者网页引用一个资源的时候，
+ * HTTP请求的会带有refer表示来源网页的URL
+ *
+ */
+var server = http.createServer(function(req,res){
+    var urlObj = url.parse(req.url,true);
+    var pathname = urlObj.pathname;
+    //Accept-Language:zh-CN,zh;q=0.8
+    if(pathname == '/favicon.ico'){
+        return res.end('404');
+    }
+    if(pathname =='/'){
+        var accept = req.headers['accept-language'];
+        var langs = accept.toLowerCase().split(',').map(function(item){
+            var s = item.split(';');
+            var lan = s[0];
+            var q = s[1]|| 'q=1';
+            if(q.substr(0,2)=='q='){
+                q = Number(q.substr(2));
+            }else{
+                q = 0;
+            }
+            return {lan:lan,q:q};
+        }).sort(function(a,b){
+            return b.q - a.q;// [{lan:'zh-cn',q:1},{lan:'zh',q:0.8}]
+        });
+        var mylangs = ['zh'];
+        var defaultLan = null;
+        //[{lan:'zh-cn',q:1},{lan:'zh',q:0.8}]
+        for(var i=0;i<langs.length;i++){
+            if(mylangs.indexOf(langs[i]['lan'])>-1){
+                defaultLan = langs[i]['lan'];
+                break;
+            }
+        }
+        fs.createReadStream('./img_'+defaultLan+'.html').pipe(res);
+    }
+
+}).listen(8080);
